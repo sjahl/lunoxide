82288a93f42feafcb7fe7c9a554ad5a6e5de7401
sjahl <636687+sjahl@users.noreply.github.com>
2023-12-20 21:36:39 -0500

    make API request and serialize response as json

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use clap::Parser;
 
 use reqwest::blocking::Client;
 
+use serde_json::Value;
+
 // Plan
 // 3. request the current moon phase and return it
 
@@ -24,13 +26,34 @@ struct Args {
     api_client_secret: String,
 }
 
+struct BasicAuth {
+    user: String,
+    password: String,
+}
+
+fn make_api_request(from_date: String, to_date: String, creds: BasicAuth) -> Result<Value, reqwest::Error> {
+    let client = Client::new();
+
+    let url = format!("https://api.astronomyapi.com/api/v2/bodies/positions/moon?latitude=42.3&longitude=-70.5&elevation=1&from_date={from_date}&to_date={to_date}&time=20:34:00");
+    
+    let resp: serde_json::Value = client.get(url)
+    .basic_auth(creds.user, Some(creds.password))
+    .send()?
+    .json()?;
+
+    Ok(resp)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
 
     let args = Args::parse();
 
+    let creds = BasicAuth{ user: args.api_client_id, password: args.api_client_secret };
+
+    let api_response = make_api_request("2023-12-01".to_string(), "2023-12-03".to_string(), creds);
+
     match args.phase {
-        true => println!("Checking the moon phase... done."),
+        true => println!("{:?}", api_response),
         false => (),
     };
 
@@ -38,16 +61,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         true => println!("Calculating the next full moon date.... done."),
         false => (),
     };
-
-
-    let client = Client::new();
-    
-    let body = client.get("https://api.astronomyapi.com/api/v2/bodies/positions/moon?latitude=42.3&longitude=-70.5&elevation=1&from_date=2023-12-01&to_date=2023-12-01&time=20:34:00")
-    .basic_auth(args.api_client_id, Some(args.api_client_secret))
-    .send()?
-    .text()?;
-
-    println!("body = {:?}", body);
 
     Ok(())
 
