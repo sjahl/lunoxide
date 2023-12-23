8a1f136108fccf531930f25a066ac26dfec08043
sjahl <636687+sjahl@users.noreply.github.com>
2023-12-23 10:25:58 -0500

    use math to calculate the phase

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use chrono::{prelude::*, Duration};
+use chrono::{prelude::*, Duration, Utc};
 use clap::Parser;
-use reqwest::blocking::Client;
-use serde_json::Value;
-use std::error::Error;
 
 // Plan
 // 3. request the current moon phase and return it
@@ -16,36 +13,32 @@ struct Args {
 
     #[arg(short, long, default_value_t = 0)]
     forecast: u8,
-
-    #[arg(long, env, hide_env = false)]
-    api_client_id: String,
-
-    #[arg(long, env, hide_env = true)]
-    api_client_secret: String,
 }
 
-struct BasicAuth {
-    user: String,
-    password: String,
+fn calc_moon_age(current_date: DateTime<Local>) -> f64 {
+    let known_new_moon = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
+    let duration: Duration = current_date.with_timezone(&Utc) - known_new_moon;
+
+    // it is highly unlikely that we will overflow an i64 with the number of days since
+    // jan 1 1900
+    #[allow(clippy::cast_precision_loss)]
+    {
+        duration.num_days() as f64 % 29.53059
+    }
 }
 
-fn make_api_request(
-    from_date: &String,
-    to_date: &String,
-    time: &String,
-    creds: BasicAuth,
-) -> Result<Value, reqwest::Error> {
-    let client = Client::new();
-
-    let url = format!("https://api.astronomyapi.com/api/v2/bodies/positions/moon?latitude=42.3&longitude=-70.5&elevation=1&from_date={from_date}&to_date={to_date}&time={time}");
-
-    let resp: serde_json::Value = client
-        .get(url)
-        .basic_auth(creds.user, Some(creds.password))
-        .send()?
-        .json()?;
-
-    Ok(resp)
+fn current_phase(moon_age: f64) -> &'static str {
+    println!("{moon_age:?}");
+    match moon_age {
+        0.0..=24.9 => "Waxing Crescent",
+        25.0..=25.9 => "First Quarter",
+        26.0..=49.9 => "Waxing Gibbous",
+        50.0..=50.9 => "Full Moon",
+        51.0..=74.9 => "Waning Gibbous",
+        75.0..=75.9 => "Last Quarter",
+        76.0..=100.0 => "Waning Crescent",
+        _ => "unknown",
+    }
 }
 
 fn phase_to_emoji(phase: &str) -> &str {
@@ -62,44 +55,22 @@ fn phase_to_emoji(phase: &str) -> &str {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let args = Args::parse();
 
-    let creds = BasicAuth {
-        user: args.api_client_id,
-        password: args.api_client_secret,
-    };
-
     let current_date: DateTime<Local> = Local::now();
-    let forecast_end_date: DateTime<Local> =
-        current_date + Duration::days(i64::from(args.forecast));
-    let date_formatted = format!("{}", current_date.format("%Y-%m-%d"));
-    let f_end_date_formatted = format!("{}", forecast_end_date.format("%Y-%m-%d"));
-    let time_formatted = format!("{}", current_date.format("%H:%M:%S"));
+    // let forecast_end_date: DateTime<Local> =
+    // current_date + Duration::days(i64::from(args.forecast));
 
-    let api_response = make_api_request(
-        &date_formatted,
-        &f_end_date_formatted,
-        &time_formatted,
-        creds,
-    )?;
-
-    let current_phase =
-        &api_response["data"]["table"]["rows"][0]["cells"][0]["extraInfo"]["phase"]["string"];
-    let current_phase = current_phase.as_str().unwrap_or_default();
+    let moon_age = calc_moon_age(current_date);
 
     if args.phase {
-        println!("{}", phase_to_emoji(current_phase));
+        println!(
+            "{}",
+            phase_to_emoji(current_phase((&moon_age / 29.53059) * 100.0))
+        );
     }
 
-    // I think the data is returned in a format like this
-
-    /*
-            | 2023-12-21T20:34:00.000-05:00 | 2023-12-21T20:34:00.000-05:00 |
-       moon || date | extraInfo | position ||| date | extraInfo | position ||
-    */
-
-    // for cell in first row of response data,
     match args.forecast {
         1..=30 => {
             println!(
@@ -110,6 +81,4 @@ fn main() -> Result<(), Box<dyn Error>> {
         31.. => println!("i only fetch forecasts of up to 30 days."),
         0 => (),
     };
-
-    Ok(())
 }
